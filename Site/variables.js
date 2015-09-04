bbd716561e37aa44e87c5c83892c752b8b0306ac
nomailme <iskander@jarmuhametov.com>
2015-09-04 13:25:33 +0300

    * variables.js ipAddress updated

--- Site/variables.js
+++ Site/variables.js
@@ -1,2 +1,2 @@
-//var ipAddress = "http://54.245.80.130:80/spity";
-var ipAddress = "http://localhost:8090/spity";
+var ipAddress = "http://54.245.80.130:80/spity";
+//var ipAddress = "http://localhost:8090/spity";
